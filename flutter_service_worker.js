d447d1cdb4d9143718e2de5b7083d64cdc3a3c7f
ComputerScienceHayk <haikazbashoyan@gmail.com>
2023-03-14 20:56:25 +0400

    a

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e024c895674e1afbaa0b609e32c04828",
-"index.html": "75c47166949e0d6d787e646e6e5b07b3",
-"/": "75c47166949e0d6d787e646e6e5b07b3",
-"main.dart.js": "a8de2c44fa747fc26222e4a3ef140e8d",
+"index.html": "e7cd2607016bd950d433c1461b3fdece",
+"/": "e7cd2607016bd950d433c1461b3fdece",
+"main.dart.js": "340152ed6ff67120cf6e7d2b3d926b6d",
 "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "favicon.png": "9014a18d5103a222a0f3b0bc9a281ca9",
 "icons/Icon-192.png": "697e655c2611cd822245422c7cd92d93",
